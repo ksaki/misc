006fad196d82b7c0e31ec5886ad5f06993620e50
Saki Kuzushima <sakikuzushima@gmail.com>
2019-05-06 23:35:27 -0400

    lda (prep)

---
+++ R/lda.R
@@ -0,0 +1,14 @@
+##################################################
+## Project: LDA - variational inference and EM
+## Script purpose: 
+## Date: 2019/5/6
+## Author: Saki Kuzushima
+## Version: 1
+##################################################
+
+## Initialize hyper parameters
+
+## function to update docs
+
+## EM
+

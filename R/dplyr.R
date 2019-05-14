af8dd7031df100af3762d892e77e6e132084fd3c
Saki Kuzushima <sakikuzushima@gmail.com>
2019-05-14 17:02:43 -0400

    binary in dplyr

--- R/dplyr.R
+++ R/dplyr.R
@@ -1,3 +1,12 @@
 ## pass strings to group_by()
 
 
+## mutate binary variables created from a categorical variable
+## Reference: https://stackoverflow.com/questions/35663580/using-tidyr-spread-function-to-create-columns-with-binary-value
+
+df <- data.frame(id = seq(1,7), day = c("Mon","Tue", "Wed","Thur", "Fri", "Sat", "Sun"))
+
+df %>%
+  mutate(var = 1) %>%
+  spread(day, var, fill = 0, sep = "_") 
+
